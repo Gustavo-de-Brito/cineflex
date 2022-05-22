2faf36ddadf531f1e1797060bb83629766e2783c
Gustavo-de-Brito <gustavoaraujo086@gmail.com>
2022-05-22 19:32:32 -0300

    adiciona botao para voltar a pagina anterior

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,8 +1,16 @@
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 
 export default function TopBar() {
+  const navigate = useNavigate();
+
+  function returnBackPage() {
+    navigate(-1);
+  }
+
   return (
     <NavBar>
+      <button onClick={ returnBackPage }>Voltar</button>
       <h1>CINEFLEX</h1>
     </NavBar>
   );
@@ -21,5 +29,18 @@ const NavBar = styled.div`
   h1 {
     font-size: 34px;
     color: #E8833A;
+  }
+
+  button {
+    position: absolute;
+    top: 4px;
+    left: 4px;
+    padding: 10px 20px;
+    background-color: #E8833A;
+    border-radius: 4px;
+    border: none;
+    color: #FFFFFF;
+    text-align: center;
+    font-size: 12px;
   }
 `;
